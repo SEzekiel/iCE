5fa024333bae3d1fe88b4915430cd8035573bd14
razakadam74 <razakadam74@gmail.com>
2017-03-15 22:12:17 +0000

    MVC folders added with empty phps

--- iCE/index.php
+++ iCE/index.php
@@ -10,30 +10,30 @@
 
 </head>
 <body>
-<div style="margin-top: 20%">
-	<div>
-		
-	<form action="" method="get" accept-charset="utf-8">
-		<label for="textinput-4" class="ui-hidden-accessible">Username or phone:</label>
-<input type="text" name="textinput-4" id="textinput-4" placeholder="Username or phone" value="" style="height: 50px"><p></p>
-		<label for="textinput-4" class="ui-hidden-accessible"> Password:</label>
-		<input type="password" name="textinput-4" id="textinput-4" placeholder="Password" value="" style="height: 50px"><p></p>
-		<div class="ui-input-btn ui-btn ui-corner-all">
-Login
-        <input type="button" data-enhanced="true" value="Login">
-    </div>
-		<div class="ui-input-btn ui-btn ui-corner-all">
-Cancel
-        <input type="button" data-enhanced="true" value="Cancel">
-    </div>
+	<div style="margin-top: 20%">
+		<div>
+			
+			<form action="" method="get" accept-charset="utf-8">
+				<label for="textinput-4" class="ui-hidden-accessible">Username or phone:</label>
+				<input type="text" name="textinput-4" id="textinput-4" placeholder="Username or phone" value="" style="height: 50px"><p></p>
+				<label for="textinput-4" class="ui-hidden-accessible"> Password:</label>
+				<input type="password" name="textinput-4" id="textinput-4" placeholder="Password" value="" style="height: 50px"><p></p>
+				<div class="ui-input-btn ui-btn ui-corner-all">
+					Login
+					        <input type="button" data-enhanced="true" value="Login">
+				    </div>
+				<div class="ui-input-btn ui-btn ui-corner-all">
+					Cancel
+					        <input type="button" data-enhanced="true" value="Cancel">
+				    </div>
 			</form>
-	</div><p>
-	<div style="text-align: center;">
-		<a href="signup.php">Don't have an account? Sign up here</a><p></p>
-		<a href="#">Forgot password</a>
+		</div><p>
+		<div style="text-align: center;">
+			<a href="signup.php">Don't have an account? Sign up here</a><p></p>
+			<a href="#">Forgot password</a>
+		</div>
 	</div>
-</div>
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 </body>
 </html>
