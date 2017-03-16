38a2e627887508749c38bf97940e7256621d95d9
PagesCoffy <isaac.p.coffie@gmail.com>
2017-03-16 19:41:15 +0000

    main model added

--- iCE/Model/dbconnect.php
+++ iCE/Model/dbconnect.php
@@ -5,7 +5,7 @@
 	define('PASSWORD', "");
 	define('DBNAME', "Emergency");
 
-	$con = mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME);
+	$conn = mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME);
 	if (mysqli_connect_errno()) {
 		die("Error". $con->connect_error)
 	}
---
+++ iCE/Model/mainmodel.php
@@ -0,0 +1,35 @@
+<?php
+include("dbconnect.php");
+
+/*returns true if connection is okay else returns false*/
+function connectionStatus(){
+	return $conn;
+}
+
+/*adds a row into a table and returns true if query was executed else returns false*/
+function executeQuery($query){
+
+if(connectionStatus()== true)
+$result = mysqli_query($conn, $query);
+
+return $result;
+}
+
+
+/*returns a resultset: an object*/
+function getResultSet($query){
+	if(connectionStatus()== true){
+       $records = mysqli_query($conn, $query);
+
+        if(mysqli_num_rows($records) > 0) {
+            return $records;
+        }
+    
+        else{
+            return null;
+        }
+
+    }
+}    
+
+?>
