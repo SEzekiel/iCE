848f72fdfb4ba8aca5079f3506a3447e7b0a0d40
razakadam74 <razakadam74@gmail.com>
2017-03-17 01:03:22 +0000

    Class headers removed

--- iCE/Controller/signup_controller.php
+++ iCE/Controller/signup_controller.php
@@ -1,15 +1,16 @@
 <?php
 
 include '../Model/User.php';
-class SignUpController
+
+
+function SignUpController($name, $gender, $phone, $email, $password, $emergencyNumber, $emergencyName)
 {
-    function SignUpController($name, $gender, $phone, $email, $password, $emergencyNumber, $emergencyName){
-        if (register($name, $gender, $phone, $email, $password, $emergencyNumber, $emergencyName)){
-            echo "true";
-        }else{
-            echo "false";
-        }
+    if (register($name, $gender, $phone, $email, $password, $emergencyNumber, $emergencyName)) {
+        echo "true";
+    } else {
+        echo "false";
     }
 }
+
 
 ?>
