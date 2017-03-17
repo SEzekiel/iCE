cf6080897ffd539b9e5a320f146e9017d0278db1
Oliver Mensah <olivermensah96@gmail.com>
2017-03-17 02:59:08 +0000

    update and upload image

--- iCE/Model/emergency_model.php
+++ iCE/Model/emergency_model.php
@@ -21,7 +21,42 @@ function get_emergency($reporter_type)
         return false;
     }
 }
-
+//upload image
+ public function uploadImage()
+  {
+    $target_dir = "images/";
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    // Check if file already exists
+    if(file_exists($target_file)) {
+      echo "Sorry, file already exists";
+      $uploadOk = 0;
+    }
+    // Check file size
+    if($_FILES["image"]["size"] > 500000) {
+      echo "Sorry, your file is too large.";
+      $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+      echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+      $uploadOk = 0;
+      }
+      // Check if $uploadOk is set to 0 by an error
+      if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+          if (
+            move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+            return true;
+            } else {
+              echo "Sorry, there was an error uploading your file.";
+           }
+       }
+  }
 
 function add_emergency($reporterNumber, $type, $recipient, $message, $lat, $long, $image)
 {
@@ -40,6 +75,25 @@ function add_emergency($reporterNumber, $type, $recipient, $message, $lat, $long
     }
 }
 
+function update_emergency($reporterNumber =null, $type=null, $recipient=null, $message=null, $lat=null, $long=null, $image=null)
+ {
+   $stmt = $conn->prepare("UPDATE emergency SET reporterNumber=:reporterNumber, type=:type, recipient=:repicient, message=:message, lat =:lat, long=:long, image=:image");
+   if($reporterNumber != null){
+     $stmt->bindparam(":reporterNumber", $reporterNumber);
+     $stmt->bindparam(":type", $type);
+     $stmt->bindparam(":recipient",$recipient);
+     $stmt->bindparam(":message",$message);
+     $stmt->bindparam(":lat",$lat);
+     $stmt->bindparam(":long",$long);
+     $stmt->bindparam(":image",$emage);
+     if($stmt->execute){
+       return true;
+     }else{
+       return false;
+     }
+   }
+
+ }
 function delete_emergency($reporter)
 {
     $query = $conn->query("DELETE FROM emergency WHERE username = '$reporter'");
@@ -49,5 +103,3 @@ function delete_emergency($reporter)
         return false;
     }
 }
-
-
