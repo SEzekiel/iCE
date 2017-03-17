011ec04c668d1ed8e70b917cb4f35ef05e34ce62
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-03-17 05:11:39 +0000

    Sending functionality

--- iCE/View/script.js
+++ iCE/View/script.js
@@ -134,32 +134,363 @@ function validateEmail(email){
 	}
 }
 
-function send(type, number){
-	var image = ""
+function sendRobbery(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is a Robbery emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
 	var choice = confirm("Do you want to attach an image to this report?");
 	if (choice == true){
 	document.getElementById('cam').click();
 	image = document.getElementById('cam').value;
 	}
 
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Robbery"+"&number=+233270327467";
 	/*
-	*Get location
+	*send the info
 	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
 
 }
 
-function getLocation() {
+function getLocation(id) {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        switch(id) {
+    case 1:
+    	navigator.geolocation.getCurrentPosition(sendRobbery);
+    case 2:
+        navigator.geolocation.getCurrentPosition(sendHealth);
+        break;
+    case 3:
+        navigator.geolocation.getCurrentPosition(sendAccident);
+        break;
+    case 4:
+        navigator.geolocation.getCurrentPosition(sendFire);
+        break;
+   	case 5:
+        navigator.geolocation.getCurrentPosition(sendFlood);
+        break;
+    case 6:
+        navigator.geolocation.getCurrentPosition(sendShock);
+        break;
+    case 7:
+        navigator.geolocation.getCurrentPosition(sendLandslide);
+        break;
+    case 8:
+        navigator.geolocation.getCurrentPosition(sendHazard);
+        break;
+    case 9:
+        navigator.geolocation.getCurrentPosition(sendNuclear);
+        break;
+    case 10:
+        navigator.geolocation.getCurrentPosition(sendOther);
+        break;
+    default:
+        alert("Unavailable")
+}
     } else {
         alert("Geolocation is not supported by your device.");
     }
 }
-function showPosition(position) {
-    var lat = position.coords.latitude;
+
+
+function sendHealth(position){
+	var lat = position.coords.latitude;
     var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is a Health emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam1').click();
+	image = document.getElementById('cam1').value;
+	}
 
-    var coord = [lat, long];
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Health"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
 
-    return coord
+}
+
+//***************************************
+
+function sendAccident(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is a Accident emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam2').click();
+	image = document.getElementById('cam2').value;
+	}
+
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Accident"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
+
+}
+
+//**************************************************
+
+function sendFire(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is a Fire emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam3').click();
+	image = document.getElementById('cam3').value;
+	}
+
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Fire"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
+
+}
+
+//**************************************************
+function sendFlood(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is a Flood emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam4').click();
+	image = document.getElementById('cam4').value;
+	}
+
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Flood"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
+
+}
+
+//*********************************************
+function sendShock(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is an Electric shock emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam5').click();
+	image = document.getElementById('cam5').value;
+	}
+
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Electrickschock"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
+
+}
+
+//*****************************************
+function sendLandslide(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is a Land slide emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam6').click();
+	image = document.getElementById('cam6').value;
+	}
+
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Landslide"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
+
+}
+
+//****************************************
+function sendHazard(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is a Biological hazard emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam7').click();
+	image = document.getElementById('cam7').value;
+	}
+
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Hazard"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
+
+}
+
+//*********************************************
+function sendNuclear(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is a Nuclear emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam8').click();
+	image = document.getElementById('cam8').value;
+	}
+
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=Nuclear"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
+
+}
+
+//************************************
+function sendOther(position){
+	var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+	var image = "";
+	var message = "Hello,\n There is an unidentified emergency at Lat. "+lat+" and Long.\nPlease I need your help"+long;
+	var choice = confirm("Do you want to attach an image to this report?");
+	if (choice == true){
+	document.getElementById('cam9').click();
+	image = document.getElementById('cam9').value;
+	}
+
+	var data = "lat="+lat+"&long="+long+"&image="+image+"&message="+message+"&recepient="+""+"&type=other"+"&number=+233270327467";
+	/*
+	*send the info
+	*/
+	var httpRequest = new XMLHttpRequest();
+	httpRequest.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			if (this.responseText == "true") {
+				alert("Message is sent. You will get a response soon");
+			} else {
+				alert("Message cannot be sent at this time");
+			}
+			//document.getElementById('display').innerHTML = this.responseText;
+		}
+	};
+	httpRequest.open("GET", "templates/signup-controller.php?"+data,true);
+	httpRequest.send();
+
 }
