3827184fd0f1942d792908d1a50c9199da49f449
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-03-25 11:53:15 +0000

    Late update for revisit

---
+++ iCE/View/templates/signup_controller.php
@@ -0,0 +1,25 @@
+<?php
+
+include '../../Model/frontend_user.php';
+$name = $_GET['name'];
+$gender = $_GET['gender'];
+$phone = $_GET['phone'];
+header('location:apage.php?name='.$name);
+$email = $_GET['email'];
+$password = $_GET['pass'];
+$emergencyNumber = $_GET['cname'];
+$emergencyName = $_GET['cnumber'];
+//signup_controller.php?name=Ezekiel&gender=Male&phone=0270327467&email=ezekielsebastine@gmail.com&pass=123&cname=Eze&cnumber=1234567891
+SignUpController($name, $gender, $phone, $email, $password, $emergencyNumber, $emergencyName);
+    
+function SignUpController($name, $gender, $phone, $email, $password, $emergencyNumber, $emergencyName)
+{
+    if (register_frontend($name, $gender, $phone, $email, $password, $emergencyNumber, $emergencyName)) {
+        echo "true";
+    } else {
+        echo "false";
+    }
+}
+
+
+?>
